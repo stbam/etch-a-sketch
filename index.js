850dd89ae24a4f8c579d58125af5d53e3b0448e8
stbam <55661516+stbam@users.noreply.github.com>
2023-01-12 04:26:34 -0500

    added editable square count

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const container = document.getElementById("grids");
+ grid_cont=document.getElementById("grid-container");
+
+
 
 for (let i=0;i<256;i++){
         const node=document.createElement("grid-item");
-        node.innerHTML="x";
+        node.innerHTML=i;
         container.appendChild(node);
         node.addEventListener("mouseenter",(event)=>{
             event.target.style.backgroundColor = "purple";
@@ -10,16 +13,18 @@ for (let i=0;i<256;i++){
 }
 
 
+
 function buttonFunction(){
    let length= prompt("Enter new length of sketchbox");
     let height=prompt("Enter the new height of sketchbox")
-    container.removeChild(); //removes the contaner with
-   
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+      }
 
+      grid_cont.style.width= `${length*20}px`;
+      
 
-
-
-       /* for (let i=0;i<length;i++){
+        for (let i=0;i<length;i++){
             for(let j=0;j<height;j++){
                 const node=document.createElement("grid-item");
                 node.innerHTML="x";
@@ -29,6 +34,6 @@ function buttonFunction(){
                 });
         }
             }
-        */
+        
 
 }
