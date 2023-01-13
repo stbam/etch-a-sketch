cea8315e1fd74610ff3c193b9e5ba87ba7a5ecd0
stbam <55661516+stbam@users.noreply.github.com>
2023-01-12 21:38:40 -0500

    fixed lines between rows of boxes

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const container = document.getElementById("grids");
- grid_cont=document.getElementById("grid-container");
-
+ 
 
 
 for (let i=0;i<256;i++){
-        const node=document.createElement("grid-item");
+        var node=document.createElement("grid-item"); //made things global as opposed to const or let
        // node.innerHTML=i;
         container.appendChild(node);
+
+
         node.addEventListener("mouseenter",(event)=>{
             event.target.style.backgroundColor = "purple";
         });
 }
+
+
 
 
 
@@ -19,20 +22,31 @@ function buttonFunction(){
     let height=prompt("Enter the new height of sketchbox")
     while (container.hasChildNodes()) {
         container.removeChild(container.firstChild);
-      }
+      }/*removes old container*/
+       
+      
+      dim_count= (336)/(length*height);//wrong
+    
+ 
+       
+  
 
-      grid_cont.style.width= `${length*20}px`;
+     // grid_cont.style.width= `${length*20}px`;  
       
 
-        for (let i=0;i<length;i++){
-            for(let j=0;j<height;j++){
+        for (let i=0;i<length*height;i++){
+            
                 const node=document.createElement("grid-item");
+                node.style.width=`${dim_count}px `;
+                node.style.height=`${dim_count}px `;
+                console.log(node.style.width)
+                console.log(node.style.height)
               //  node.innerHTML="x";
                 container.appendChild(node);
                 node.addEventListener("mouseenter",(event)=>{
                     event.target.style.backgroundColor = "purple";
                 });
-        }
+        
             }
         
 
