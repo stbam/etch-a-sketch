31519c985a69a76ff13b72a88226412331240acd
stbam <55661516+stbam@users.noreply.github.com>
2023-01-13 20:15:45 -0500

    Grid can be changed now

--- index.js
+++ index.js
@@ -19,35 +19,38 @@ for (let i=0;i<256;i++){
 
 function buttonFunction(){
    let length= prompt("Enter new length of sketchbox");
-    let height=prompt("Enter the new height of sketchbox")
+    let height= length;  //prompt("Enter the new height of sketchbox")
     while (container.hasChildNodes()) {
         container.removeChild(container.firstChild);
       }/*removes old container*/
        
-      let x=length;
-      dim_count= (336)/( (2**x));//wrong
-    
+//length=2 boxes
+//height=2 boxes
 
-       
-  console.log(Math.pow(2,0))
+//dimens=640px
+ 
+     dim_count= ((320)/ ( (length)));//wrong divide 640 into equal length*height squares
+   // console.log(dim_count)
 
-     // grid_cont.style.width= `${length*20}px`;  
-      
+        for (let i=0;i<height;i++){
+            for(let j=0;j<length;j++){
 
-        for (let i=0;i<length*height;i++){
-            
                 const node=document.createElement("grid-item");
+
                 node.style.width=`${dim_count}px `;
-                node.style.height=`${dim_count}px `;
+                node.style.height=`${dim_count}px`;
+               
+                
                 console.log(node.style.width)
                 console.log(node.style.height)
-              //  node.innerHTML="x";
+              
+
                 container.appendChild(node);
                 node.addEventListener("mouseenter",(event)=>{
                     event.target.style.backgroundColor = "purple";
                 });
         
-            }
+            }}
         
 
 }
